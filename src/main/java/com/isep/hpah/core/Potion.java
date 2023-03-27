090e1e505aca8cf2498c36053bf0b1d2c84afdd5
Aelig <aelig.villiette@gmail.com>
2023-03-27 14:27:38 +0200

    lsqkjdfmlqskjfdmlqskfjkmlds

--- src/main/java/com/isep/hpah/core/Potion.java
+++ src/main/java/com/isep/hpah/core/Potion.java
@@ -1,11 +1,4 @@
 package com.isep.hpah.core;
 
 public class Potion {
-    if( sorcier.hp < sorcier.maxHp){
-        //player CAN take a potion
-    }else{
-        //player CANNOT take a potion
-
-        anythingToContinue();
-    }
 }
